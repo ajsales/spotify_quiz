4e7469bcdc9f09347cc8c7ecb830a6288725e1f0
Adrian Jules Sales <ajsales@berkeley.edu>
2020-11-29 19:06:08 -0800

    Revert back

--- app/callback/CallbackController.js
+++ app/callback/CallbackController.js
@@ -2,5 +2,5 @@ app.controller('CallbackController', function($scope, Spotify) {
 
 	var token = window.location.hash.split('&')[0].split('=')[1];
     localStorage.setItem('spotify-token', token);
-    window.close();
+    
 });
