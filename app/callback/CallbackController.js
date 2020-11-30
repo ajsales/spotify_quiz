ec3099a5522bb6b0a4c869670865ff8ab5fd9030
Adrian Jules Sales <ajsales@berkeley.edu>
2020-11-29 19:25:10 -0800

    Trying something 2

--- app/callback/CallbackController.js
+++ app/callback/CallbackController.js
@@ -3,4 +3,5 @@ app.controller('CallbackController', function($scope, Spotify) {
 	var token = window.location.hash.split('&')[0].split('=')[1];
     localStorage.setItem('spotify-token', token);
     $scope.token = token;
+    $scope.token2 = localStorage.getItem('spotify-token');
 });
