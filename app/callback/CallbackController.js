0ed5d79e9ec1120eb6c7caf165c797ad47ba7029
Adrian Jules Sales <ajsales@berkeley.edu>
2020-12-02 20:39:03 -0800

    Trying to see where the error is

--- app/callback/CallbackController.js
+++ app/callback/CallbackController.js
@@ -2,6 +2,4 @@ app.controller('CallbackController', function($scope, Spotify) {
 
 	var token = window.location.hash.split('&')[0].split('=')[1];
     localStorage.setItem('spotify-token', token);
-    $scope.token = token;
-    $scope.token2 = localStorage.getItem('spotify-token');
 });
