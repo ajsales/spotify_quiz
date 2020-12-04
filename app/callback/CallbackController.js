ad20fdff182672dec0316a8136d1d2462364c3d9
Adrian Jules Sales <ajsales@berkeley.edu>
2020-12-04 01:30:32 -0800

    Adding error stuff

--- app/callback/CallbackController.js
+++ app/callback/CallbackController.js
@@ -11,25 +11,25 @@ app.controller('CallbackController', function($scope, spotify, $location) {
 
 		// Get user's top 10 recent songs.
 		return spotify.getMyTopTracks({limit: 10, time_range: 'short_term'});
-	}, () => console.log("Couldn't get profile stuff."))
+	}, (err) => console.log(err))
 	.then((res) => {
 		user.recent_songs = res.items;
 
 		// Get user's top 10 all-time songs.
 		return spotify.getMyTopTracks({limit: 10, time_range: 'long_term'});
-	}, () => console.log("Couldn't get top tracks (short-term)."))
+	}, (err) => console.log(err))
 	.then((res) => {
 		user.alltime_songs = res.items;
 
 		//Get user's top 10 recent artists.
 		return spotify.getMyTopArtists({limit: 10, time_range: 'short_term'});
-	}, () => console.log("Couldn't get top tracks (long-term)."))
+	}, (err) => console.log(err))
 	.then((res) => {
 		user.recent_artists = res.items;
 
 		//Get user's top 10 all-time artists.
 		return spotify.getMyTopArtists({limit: 10, time_range: 'long_term'});
-	}, () => console.log("Couldn't get top artists (short-term)."))
+	}, (err) => console.log(err))
 	.then((res) => {
 		user.alltime_artists = res.items;
 
@@ -38,5 +38,5 @@ app.controller('CallbackController', function($scope, spotify, $location) {
 		$location.hash('');
 		$location.path('/rooms');
 		$scope.$apply();
-	}, () => console.log("Couldn't get top artists (long-term)."))
+	}, (err) => console.log(err))
 });
